435e4abea7d10541239e2420f1ea151a9c84f47c
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-04-07 18:43:13 +0100

    Removed Imperial conversion 👋

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ return days[day];
 }
 
 function displayForecast(response) {
-let forecast = response.data.daily;
+let forecast =response.data.daily;
 let forecastElement = document.querySelector("#forecast");
 
 let forecastHTML =`<div class="row">`;
@@ -109,36 +109,9 @@ function handleSubmit(event){
     let cityInputElement = document.querySelector("#city-input");
     search(cityInputElement.value);
 }
-function displayFahrenheitTemperature(event){
-    event.preventDefault();
-    let fahrenheitTemperature = (celsiusTemperature * 9)/5 +32;
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function displayCelsiusTemperature(event){
-    event.preventDefault();
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(celsiusTemperature);
-
-}
-
-
-let celsiusTemperature = null;
-
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("London");
 displayForecast();
